badb690e0c30e4089872194d08c515760150282a
Tim Lucas <t.lucas@toolmantim.com>
2011-05-16 04:16:14 +1000

    Disable the autofill robot

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,22 @@
-function autoFill() {
-  setTimeout(function() {
-    var f = $(".gate form");
-    f.find("input").val("sekrit");
-    f.submit();
-  }, 500);
-  setTimeout(function() {
-    var f = $(".form form");
-    f.find(".cut input:first").attr("checked", "checked");
-    f.find(".size input:first").attr("checked", "checked");
-    f.find("input[name='name']").val("Jesus");
-    f.find("input[name='email']").val("holy@jesus.com");
-    f.submit();
-  }, 4000);
-}
-
 $(function() {
   new Rubpocalypse.Views.Order({el:$("#order")});
-  autoFill();
+
+  // engageAutoFillRobot();
+
+  function engageAutoFillRobot() {
+    setTimeout(function() {
+      var f = $(".gate form");
+      f.find("input").val("sekrit");
+      f.submit();
+    }, 500);
+    setTimeout(function() {
+      var f = $(".form form");
+      f.find(".cut input:first").attr("checked", "checked");
+      f.find(".size input:first").attr("checked", "checked");
+      f.find("input[name='name']").val("Jesus");
+      f.find("input[name='email']").val("holy@jesus.com");
+      f.submit();
+    }, 4000);
+  }
+  
 });
