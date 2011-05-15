320488bce0e54b3cf045082b0403ac929bb26822
Tim Lucas <t.lucas@toolmantim.com>
2011-05-15 20:05:25 +1000

    Added the form validation and submit handler

--- public/js/views/form.js
+++ public/js/views/form.js
@@ -1,7 +1,9 @@
 Rubpocalypse.Views.Form = Backbone.View.extend({
+  emailPattern: /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   events: {
     "keyup input": "checkForCompletion",
-    "change input": "checkForCompletion"
+    "change input": "checkForCompletion",
+    "submit form": "transmit"
   },
   initialize: function() {
     this.checkForCompletion();
@@ -17,6 +19,25 @@ Rubpocalypse.Views.Form = Backbone.View.extend({
       submit.attr("disabled",true);
   },
   isComplete: function() {
-    return false;
+    return this.validCut() &&
+           this.validSize() && 
+           this.validName() &&
+           this.validEmail();
+  },
+  validCut: function() {
+    return this.$("input[name='cut']:checked").length;
+  },
+  validSize: function() {
+    return this.$("input[name='size']:checked").length;
+  },
+  validName: function() {
+    return $.trim(this.$("input[name='name']").val()).length;
+  },
+  validEmail: function() {
+    return this.emailPattern.test($.trim(this.$("input[name='email']").val()));
+  },
+  transmit: function(e) {
+    e.preventDefault();
+    alert("TRANSMIT!");
   }
 });
