576302e09ecd9c4a6b8ca65615cde386238cf759
Tim Lucas <t.lucas@toolmantim.com>
2011-05-17 07:16:22 +1000

    Rejig the way sounds work

--- public/js/sounds.js
+++ public/js/sounds.js
@@ -1,22 +1,9 @@
-// We want document ready, to push these file loads to the back of the queue
-(function() {
+$(function() {
 
-  var createSound = function(src, autoplay) {
-    if (!(Modernizr.audio && (Modernizr.audio.mp3 || Modernizr.audio.ogg)))
-      return {play:function() {}, pause:function() {}};
-    var audio = document.createElement("audio");
-    audio.setAttribute("preload", "auto");
-    if (autoplay) audio.setAttribute("autoplay", "true");
-    audio.setAttribute("src", "/sounds/" + src + "." + (Modernizr.audio.mp3 ? "mp3" : "ogg"));
-    return audio;
-  };
-  
-  Rubpocalypse.Sounds = {
-    crickets: createSound("crickets", Modernizr.cssanimations),
-    fireball: createSound("fireball"),
-    explosion: createSound("explosion"),
-    success: createSound("success"),
-    error: createSound("error")
-  };
-  
+  Rubpocalypse.Sounds = {};
+
+  $("audio").each(function() {
+    Rubpocalypse.Sounds[$(this).id().replace("sound-","")] = this;
+  });
+
 }());
