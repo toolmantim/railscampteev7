731cb1ff204b45b78eaece9815aa236a81fd5644
Tim Lucas <t.lucas@toolmantim.com>
2011-05-16 20:51:49 +1000

    Added obligatory PBJ

--- public/js/views/confirmation.js
+++ public/js/views/confirmation.js
@@ -2,5 +2,14 @@ Rubpocalypse.Views.Confirmation = Backbone.View.extend({
   show: function(number) {
     $(this.el).find(".number").text(number);
     $(this.el).addClass("visible");
+    setTimeout(_.bind(function() { this.showPBJ(); }, this), 10000);
+  },
+  showPBJ: function() {
+    $("<img id='pbj'>")
+      .load(function() {
+        $(this).appendTo("#order");
+        _.defer(_.bind(function() { $(this).addClass("boppin"); }, this));
+      })
+      .attr("src", "/images/pjb-banana.gif");
   }
 });
