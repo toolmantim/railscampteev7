ddad56615f5b3b6b94f1def15914718cded51a95
Tim Lucas <t.lucas@toolmantim.com>
2011-05-16 21:04:58 +1000

    Tee jazzle and PBJ refacturbation

--- public/js/views/confirmation.js
+++ public/js/views/confirmation.js
@@ -2,14 +2,17 @@ Rubpocalypse.Views.Confirmation = Backbone.View.extend({
   show: function(number) {
     $(this.el).find(".number").text(number);
     $(this.el).addClass("visible");
-    setTimeout(_.bind(function() { this.showPBJ(); }, this), 10000);
+    setTimeout(_.bind(function() { this.showPBJ(); }, this), 15000);
   },
   showPBJ: function() {
+    this.loadPBJ(function(pbj) {
+      pbj.appendTo("#order");
+      _.defer(function() { pbj.addClass("boppin"); });
+    });
+  },
+  loadPBJ: function(callback) {
     $("<img id='pbj'>")
-      .load(function() {
-        $(this).appendTo("#order");
-        _.defer(_.bind(function() { $(this).addClass("boppin"); }, this));
-      })
+      .load(function() { callback($(this)); })
       .attr("src", "/images/pjb-banana.gif");
   }
 });
