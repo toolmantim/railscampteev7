289a8ed17b9b309bf3fbd425841066339d9b019b
Tim Lucas <t.lucas@toolmantim.com>
2011-05-18 11:07:26 +1000

    Longer mp3s to fix Webkit :(

--- public/js/sounds.js
+++ public/js/sounds.js
@@ -8,10 +8,6 @@ Rubpocalypse.Sounds = (function() {
     play: function(name) {
       if (!Modernizr.audio) return;
       var sound = soundForName(name);
-      sound.pause();
-      // Work-around for a Webkit bug:
-      // http://code.google.com/p/chromium/issues/detail?id=25972
-      sound.src = sound.src;
       sound.play();
     },
     pause: function(name) {
